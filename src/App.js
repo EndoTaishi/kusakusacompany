8697b7f4f73b27b42567ae886abb579d11590a52
遠藤大志 <taimei0405@gmail.com>
2022-06-04 18:58:54 +0900

    App.jsにstyled-componentsを追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import './App.css';
+import styled from "styled-components"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>草</h1>
-      </header>
-    </div>
+    <>
+      <AppContainer>
+        <AppHeader>
+          草草カンパニー
+        </AppHeader>
+      </AppContainer>
+    </>
   );
 }
+
+const AppContainer = styled.div`
+    text-align: center;
+`
+
+const AppHeader = styled.h1`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: greenyellow;
+    font-size: 24px;
+`
 
 export default App;
