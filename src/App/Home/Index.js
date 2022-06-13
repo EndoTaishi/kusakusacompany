54659fb3cb967146784ea8ed8522fdbd7d129c69
遠藤大志 <taimei0405@gmail.com>
2022-06-14 02:49:46 +0900

    Member.jsを変更

--- src/App/Home/Index.js
+++ src/App/Home/Index.js
@@ -1,13 +1,28 @@
-import Home from "./Index/Home";
+import styled from "styled-components"
+
 import Member from "./Index/Member.js";
 
 const Index = () => {
   return (
     <>
-      <Home />
+      <Eyecatch>草草カンパニー</Eyecatch>
       <Member />
     </>
   );
 }
+
+const Eyecatch = styled.h1`
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    font-size: 10vw;
+    font-weight: normal;
+    margin: 0;
+    min-height: 200px;
+    padding: 0;
+    height: 100vh;
+    background:linear-gradient(90deg,#D9ED92 0%,#D9ED92 80%,#99D98C 80%,#99D98C 90%, #116688 90%, #116688 100%);
+    color: black;
+`
 
 export default Index;
--- src/App/Home/Index/Member.js
+++ src/App/Home/Index/Member.js
@@ -1,68 +1,72 @@
 import styled from "styled-components"
 
+import CeoImage from "./Member/ceo.png"
+
 const Member = () => {
   return (
-    <MemberContainer>
-      <InnerContainer>
-        <h2>OFFICER</h2>
-        <FlexContainer>
-          <Profile>
-            <h3>遠藤　大志 / 代表社員</h3>
-            <p>
-              2000年05月24日生まれ<br />
-              早稲田大学人間科学部4年
-            </p>
-          </Profile>
-          <a
-            href="https://twitter.com/endotaishi_"
-            target="_blank"
-            rel="noreferrer">
-          </a>
-        </FlexContainer>
-      </InnerContainer>
-    </MemberContainer>
+    <MemberWrapper>
+        <Container>
+            <h2>社長あいさつ</h2>
+            <FlexContainer>
+                <img src={CeoImage} alt="Taishi Endo" />
+                <Profile>
+                    <h3>遠藤大志 / 代表社員</h3>
+                    <p>早稲田大学人間科学部4年</p>
+                    <p>2000年5月24日生まれ、双子座、A型、176cm、66kg</p>
+                    <p>好きな食べ物はいちご、嫌いな食べ物はナッツ類</p>
+                    <p>2022年2月末日、合同会社草草カンパニーを設立しました</p>
+                    <a href="https://twitter.com/endotaishi_" target="_blank" rel="noreferrer">Twitterはこちら</a>
+                </Profile>
+            </FlexContainer>
+        </Container>
+    </MemberWrapper>
   )
 }
 
-const MemberContainer = styled.div`
-    background-color: #eeeeee;
-    padding: 60px;
+const MemberWrapper = styled.div`
+    
 `
 
-const InnerContainer = styled.div`
-    color: white;
+const Container = styled.div`
+    color: #000000;
     margin: 0 auto;
+    padding: 40px 60px 60px 60px;
+    max-width: 1024px;
     width: 100%;
 
     h2 {
-        background-color: #59795A;
-        color: white;
-        margin-bottom: 30px;
-        padding: 20px 60px;
-        text-align: center;
+        margin: 0;
+        padding-bottom: 30px;
+        font-size: 32px;
+    }
+`
+const FlexContainer = styled.div`
+    background-color: #eeeeee;
+    display: flex;
+    align-items: center;
+    /*height: 270px;*/
+    overflow: hidden;
+    position: relative;
+
+    img{
+        margin-right: 40px;
+        width: 15vw;
     }
 `
 
-const FlexContainer = styled.div`
-    background-color: #59795A;
-    display: flex;
-    align-items: center;
-    height: 270px;
-    overflow: hidden;
-    position: relative;
-`
-
 const Profile = styled.div`
-  h3 {
-    font-weight: bold;
-    padding-bottom: 15px;
-  }
+    h3{
+        font-weight: bold;
+    }
 
-  p {
-    margin: 0;
-    opacity: 0.7;
-    text-shadow: white 0 0 0.5px;
-  }
+    p {
+        margin: 0;
+    }
+
+    a{
+        text-decoration: none;
+        color: #3498db;
+    }
 `
 
 export default Member
